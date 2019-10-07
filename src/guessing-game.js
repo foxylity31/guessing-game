23870ba988a283915bc0b60e2d4c3d28636b3845
Veronika <veronimakoroni@gmail.com>
2019-10-07 23:03:20 +0300

    add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.numberMin = min;
+        this.numberMax = max;
     }
 
     guess() {
-
+        return this.number = Math.round((this.numberMax + this.numberMin)/2);
     }
 
     lower() {
-
+       this.numberMax = this.number; 
     }
 
     greater() {
-
+        this.numberMin = this.number; 
     }
 }
 
